edbca5d810a1ee0c3c6d4358816a9cda3606c872
Tim Lenertz <timlenertz@gmail.com>
2017-05-09 19:48:57 +0200

    cg_optical_flow_slopes

---
+++ src/calibration/cg_estimate_rotation.cc
@@ -0,0 +1,89 @@
+#include <string>
+#include <map>
+#include <cmath>
+#include "lib/image_correspondence.h"
+#include "../lib/utility/misc.h"
+#include "../lib/json.h"
+#include "../lib/dataset.h"
+#include "../lib/opencv.h"
+#include "../lib/camera.h"
+
+using namespace tlz;
+
+[[noreturn]] void usage_fail() {
+	std::cout << "usage: cg_estimate_rotation dataset_parameters.json image_correspondences.json intrinsics.json out_rotation.json\n";
+	std::cout << std::endl;
+	std::exit(1);
+}
+
+struct flow_line {
+	vec2 reference_view_point;
+	real slope;
+};
+
+std::pair<real, real> estimate_pitch_roll_from_vertical_flow(const dataset& datas, const mat33& K, const image_correspondences& cors) {
+	real fx = K(0, 0), fy = K(1, 1), cx = K(0, 2), cy = K(1, 2);
+	view_index reference_idx = cors.reference;
+	
+	std::cout << "determining vertical flow lines" << std::endl;
+	std::map<std::string, flow_line> feature_flow_lines;
+	for(const auto& kv : cors.features) {
+		const std::string& feature_name = kv.first;
+		const image_correspondence_feature& feature = kv.second;
+		
+		std::vector<vec2> points;
+		for(const auto& kv2 : feature.points) points.push_back(kv.second);
+		
+		vec4 line_parameters;
+		cv::fitLine(points, line_parameters, CV_DIST_L2, 0.0, 0.01, 0.01);
+		
+		feature_flow_lines[feature_name] = flow_line {
+			feature.points.at(reference_idx),
+			line_parameters[0] / line_parameters[1]
+		};
+	}
+	
+	real predicted = [&](const std::string& feature_name, real pitch, real roll) {
+		vec2 i = feature_flow_lines.at(feature_name).reference_view_point;
+		real ix = i[0], iy = i[1];
+		real tanroll = std::tan(roll), sinpitch = std::sin(pitch), cospitch = std::cos(pitch);
+		return ((ix - cx)*sinpitch + fx*tanroll) / (fx*cospitch + (iy - cy)*sinpitch);
+	};
+	real error = [&](real pitch, real roll) {
+		real error = 0.0;
+		for(const auto& kv : feature_flow_lines) {
+			const std::string& feature_name = kv.first;
+			const flow_line& line = kv.second;
+			real predicted = predicted(feature_name, pitch, roll);
+			real observed = line.slope;
+			error += std::abs(observed - predicted);
+		}
+		return error;
+	};
+	
+	
+}
+
+int main(int argc, const char* argv[]) {
+	if(argc <= 4) usage_fail();
+	std::string dataset_parameter_filename = argv[1];
+	std::string cors_filename = argv[2];
+	std::string intrinsics_filename = argv[3];
+	std::string out_rotation_filename = argv[4];
+	
+	std::cout << "loading data set" << std::endl;
+	dataset datas(dataset_parameter_filename);
+
+	std::cout << "loading intrinsic matrix" << std::endl;
+	const json& j_intrinsics = import_json_file(intrinsics_filename);
+	mat33 K = decode_mat(j_intrinsics["K"]);
+	
+	std::cout << "loading correspondences" << std::endl;
+	image_correspondences cors = import_image_correspondences_file(cors_filename);
+
+	std::cout << "estimating pitch from vertical flow" << std::endl;
+	estimate_pitch_roll_from_vertical_flow(datas, K, cors);
+	
+	
+	std::cout << "estimating yaw from horizontal flow" << std::endl;
+}
---
+++ src/calibration/cg_optical_flow_slopes.cc
@@ -0,0 +1,72 @@
+#include <string>
+#include <map>
+#include <cmath>
+#include "lib/image_correspondence.h"
+#include "../lib/utility/misc.h"
+#include "../lib/json.h"
+#include "../lib/dataset.h"
+#include "../lib/opencv.h"
+#include "../lib/camera.h"
+
+using namespace tlz;
+
+[[noreturn]] void usage_fail() {
+	std::cout << "usage: optical_flow_slopes dataset_parameters.json image_correspondences.json out_slopes.json\n";
+	std::cout << std::endl;
+	std::exit(1);
+}
+int main(int argc, const char* argv[]) {
+	if(argc <= 3) usage_fail();
+	std::string dataset_parameter_filename = argv[1];
+	std::string cors_filename = argv[2];
+	std::string out_slopes_filename = argv[3];
+	
+	std::cout << "loading data set" << std::endl;
+	dataset datas(dataset_parameter_filename);
+	
+	std::cout << "loading correspondences" << std::endl;
+	image_correspondences cors = import_image_correspondences_file(cors_filename);
+	view_index reference_idx = cors.reference;
+
+	
+	std::cout << "estimating slopes for horizontal flow" << std::endl;
+	std::map<std::string, real> feature_horizontal_slopes;
+	for(const auto& kv : cors.features) {
+		const std::string& feature_name = kv.first;
+		const image_correspondence_feature& feature = kv.second;
+		
+		std::vector<cv::Vec2f> points;
+		for(int x : datas.x_indices()) points.push_back(feature.points.at(view_index(x, reference_idx.y)));
+		
+		cv::Vec4f line_parameters;
+		cv::fitLine(points, line_parameters, CV_DIST_L2, 0.0, 0.01, 0.01);
+				
+		feature_horizontal_slopes[feature_name] = line_parameters[1] / line_parameters[0];
+	}
+
+	std::cout << "estimating slopes for vertical flow" << std::endl;
+	std::map<std::string, real> feature_vertical_slopes;
+	for(const auto& kv : cors.features) {
+		const std::string& feature_name = kv.first;
+		const image_correspondence_feature& feature = kv.second;
+		
+		std::vector<cv::Vec2f> points;
+		for(int y : datas.y_indices()) points.push_back(feature.points.at(view_index(reference_idx.x, y)));
+		
+		cv::Vec4f line_parameters;
+		cv::fitLine(points, line_parameters, CV_DIST_L2, 0.0, 0.01, 0.01);
+				
+		feature_vertical_slopes[feature_name] = line_parameters[0] / line_parameters[1];
+	}
+	
+	std::cout << "saving slopes" << std::endl;
+	json j_slopes = json::object();
+	for(const auto& kv : cors.features) {
+		const std::string& feature_name = kv.first;
+		json j_slope = json::object();
+		j_slope["horizontal"] = feature_horizontal_slopes.at(feature_name);
+		j_slope["vertical"] = feature_vertical_slopes.at(feature_name);
+		j_slopes[feature_name] = j_slope;
+	}
+	export_json_file(j_slopes, out_slopes_filename);
+}
