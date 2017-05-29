30e534e3a2cad044a6b1a698b52a1a2b5dd4ef88
Tim Lenertz <timlenertz@gmail.com>
2017-05-30 00:13:37 +0200

    ...

--- src/misc/copy_json.cc
+++ src/misc/copy_json.cc
@@ -1,19 +1,13 @@
+#include "../lib/args.h"
 #include "../lib/json.h"
-#include <iostream>
-#include <cstdlib>
 
 using namespace tlz;
 
-[[noreturn]] void usage_fail() {
-	std::cout << "usage: copy_json in.json out.json" << std::endl;
-	std::exit(1);
-}
-
 int main(int argc, const char* argv[]) {
-	if(argc <= 2) usage_fail();
-	std::string in_filename = argv[1];
-	std::string out_filename = argv[2];
-	
+	get_args(argc, argv, "in.json out.json");
+	std::string in_filename = in_filename_arg();
+	std::string out_filename = out_filename_arg();
+
 	json j = import_json_file(in_filename);
 	export_json_file(j, out_filename);
 }
