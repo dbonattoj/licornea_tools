cb9ebde1c4ab2a73991a2494ab3c8077f889d6a7
Tim Lenertz <timlenertz@gmail.com>
2017-07-24 00:45:36 +0200

    report

--- src/lib/pylib/config.py
+++ src/lib/pylib/config.py
@@ -7,5 +7,9 @@ parallel = True
 # If parallel if True, number of parallel jobs to launch.
 # Should be adjusted to get best efficiency. For jobs that access a lot of different files, should be low,
 # otherwise time is lost seeking the disk drive.
+<<<<<<< Updated upstream
 parallel_jobs = 3
+=======
+parallel_jobs = 5
+>>>>>>> Stashed changes
 
