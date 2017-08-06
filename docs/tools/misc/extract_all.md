62344398e53cd861054f775376cf0bb221ed0129
Tim Lenertz <timlenertz@gmail.com>
2017-08-06 20:47:46 +0200

    extract_all

---
+++ docs/tools/misc/extract_all.md
@@ -0,0 +1,7 @@
+# misc/extract\_all
+
+Copy sequence of 1D dataset images.
+
+    misc/extract_all parameters.json out_dirname/
+
+Copies the existing images from the dataset into files in `out_dirname/` with sequentially numbered names.
---
+++ src/misc/extract_all.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python
+from pylib.dataset import *
+import os, json, math, sys, shutil
+
+
+def usage_fail():
+	print("usage: {} parameters.json out_dirname/\n".format(sys.argv[0]))
+	sys.exit(1)
+
+if len(sys.argv) <= 3: usage_fail()
+parameters_filename = sys.argv[1]
+out_dirname = sys.argv[2]
+
+datas = Dataset(parameters_filename)
+if not datas.is_1d(): raise Exception("dataset must be 1D")
+
+out_frame = 0
+for x in datas.x_indices():
+	view = datas.view(x)
+	image_filename = view.image_filename()
+	if not os.path.isfile(image_filename): continue
+
+	out_frame = out_frame + 1
+	out_image_filename = os.path.join(out_dirname, "frame_{:05d}.png".format(out_frame))
+	shutil.copyfile(image_filename, out_image_filename)
+	print("{} of {}".format(out_frame+1,len(images)))
